c50f92b2ce3a897f88af57bc6054613c50a69c8d
Darwin <darwinu34@yahoo.es>
2020-05-10 08:27:54 -0500

    se crea el usuario post

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 /* Requieres */
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
 /* Inicializar variables */
 var app = express();
 
+//Body parser es un milleware para utilizar objeto de java scrip en cualquier lugar.
+// parse application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Importar rutas
-var appRoutas = require('./routes/app');
-var usuarioRoutas = require('./routes/usuario');
+var appRoutas = require("./routes/app");
+var usuarioRoutas = require("./routes/usuario");
 
 mongoose.connection.openUri(
     "mongodb://localhost:27017/hospital-db",
@@ -20,11 +27,10 @@ mongoose.connection.openUri(
 );
 
 //Rutas
-app.use('/usuario', usuarioRoutas);
-app.use('/', appRoutas);
+app.use("/usuario", usuarioRoutas);
+app.use("/", appRoutas);
 
-
-/* DUM: servidor corre en el puerto 3000, comando node app */
+/* DUM: servidor corre en el puerto 3000, comando node app, los caracteres es para dar color a online */
 app.listen(3000, () => {
     console.log("Express server puerto 3000: \x1b[32m%s\x1b[0m", " online");
 });
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,28 +1,55 @@
-var express = require('express');
+var express = require("express");
 var app = new express();
 
-var Usuario = require('../models/usuario');
+var Usuario = require("../models/usuario");
 
-
+/* Obtener todos los usuarios */
 app.get("/", (request, respuesta, next) => {
+    /* DUM para que solo devuelva las columnas que se especifican excec */
+    Usuario.find({}, "nombre apellidos email img role").exec(
+        (error, usuarios) => {
+            if (error) {
+                return respuesta.status(500).json({
+                    ok: false,
+                    mensaje: "Error cargando usuario base de datos",
+                    errors: error,
+                });
+            }
 
-    Usuario.find({}, (error, usuarios) => {
+            respuesta.status(200).json({
+                ok: true,
+                usuarios: usuarios,
+            });
+        }
+    );
+});
 
+/* Crear un nuevo usuario */
+app.post("/", (req, res) => {
+    /* solo funciona con la libreria body parse */
+    var body = req.body;
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        apellidos: body.apellidos,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((error, usuarioGuardado) => {
         if (error) {
             return respuesta.status(500).json({
                 ok: false,
-                mensaje: "Error cargando usuario base de datos",
-                errors: error
+                mensaje: "Error al crear usuario en la base de datos",
+                errors: error,
             });
         }
-
-        respuesta.status(200).json({
+        res.status(201).json({
             ok: true,
-            usuarios: usuarios
+            body: usuarioGuardado,
         });
-
     });
-
 });
 
 module.exports = app;
