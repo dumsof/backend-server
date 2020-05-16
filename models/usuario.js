94e1c2c8e0475fc3b1fba5e731f4bba4322c7a1e
Darwin <darwinu34@yahoo.es>
2020-05-16 07:21:43 -0500

    se inicia la implementación de guardar actualizar.

--- models/usuario.js
+++ models/usuario.js
@@ -1,18 +1,21 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
+var Schema = mongoose.Schema;
 
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+};
 
-var Shema = mongoose.Schema;
-
-var usuairoShema = new Shema({
+var usuairoShema = new Schema({
     nombre: { type: String, require: [true, 'El nombre es necesario'] },
     apellidos: { type: String, require: [true, 'Los apellidos son necesario'] },
     email: { type: String, unique: true, require: [true, 'El email es necesario'] },
     password: { type: String, require: [true, 'La contraseña es necesario'] },
-    img: { type: String },
-    role: { type: String, require: true, default: 'USER_ROLE' }
+    img: { type: String, require: false },
+    role: { type: String, require: true, default: 'USER_ROLE', enum: rolesValidos }
 });
-usuairoShema.plugin(uniqueValidator, { message: 'El {PATH} debe ser unico.' });
 
+usuairoShema.plugin(uniqueValidator, { message: 'El {PATH} debe ser unico.' });
 module.exports = mongoose.model('Usuario', usuairoShema);
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var bcrypt = require('bcrypt');
 var app = new express();
 
 var Usuario = require("../models/usuario");
@@ -32,23 +33,66 @@ app.post("/", (req, res) => {
         nombre: body.nombre,
         apellidos: body.apellidos,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
 
-    usuario.save((error, usuarioGuardado) => {
-        if (error) {
-            return respuesta.status(500).json({
+    usuario.save((errores, usuarioGuardado) => {
+        if (errores) {
+            return respuesta.status(400).json({
                 ok: false,
                 mensaje: "Error al crear usuario en la base de datos",
-                errors: error,
+                errors: errores,
             });
         }
         res.status(201).json({
             ok: true,
             body: usuarioGuardado,
         });
+    });
+});
+
+/* Actualizar usuario */
+app.put("/:id", (req, res) => {
+    /* solo funciona con la libreria body parse */
+    var id = req.params.id;
+    Usuario.findById(id, (error, usuario) => {
+        if (errores) {
+            return respuesta.status(500).json({
+                ok: false,
+                mensaje: "Error Base de Datos, Buscar usuario Actualizar",
+                errors: errores,
+            });
+        }
+        if (!usuario) {
+            return respuesta.status(400).json({
+                ok: false,
+                mensaje: "El usuario no existe",
+                errors: { message: 'No existe un usuario con ese ID' },
+            });
+        }
+        var body = req.body;
+
+        usuario.nombre = body.nombre;
+        usuario.apellidos = body.apellidos;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((errores, usuarioGuardado) => {
+            if (errores) {
+                return respuesta.status(400).json({
+                    ok: false,
+                    mensaje: "Error al actualizar usuario",
+                    errors: errores,
+                });
+            }
+            res.status(201).json({
+                ok: true,
+                body: usuarioGuardado,
+            });
+        });
+
     });
 });
 
