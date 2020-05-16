46da219f6d4c2797c53feae93ed2dd55a3529ea6
Darwin <darwinu34@yahoo.es>
2020-05-16 16:07:53 -0500

    se agrega midleware se solucionan los problemas.

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,12 @@
 var express = require("express");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 var app = new express();
+
+/* MedlleWare */
+/* Esto permite que pueda leer en el post json */
+app.use(express.json());
+/* Dum: funcion para ver la ruta solicitada */
+app.use(logger);
 
 var Usuario = require("../models/usuario");
 
@@ -26,7 +32,7 @@ app.get("/", (request, respuesta, next) => {
 });
 
 /* Crear un nuevo usuario */
-app.post("/", (req, res) => {
+app.post("/", (req, respuesta) => {
     /* solo funciona con la libreria body parse */
     var body = req.body;
     var usuario = new Usuario({
@@ -35,15 +41,15 @@ app.post("/", (req, res) => {
         email: body.email,
         password: bcrypt.hashSync(body.password, 10),
         img: body.img,
-        role: body.role
+        role: body.role,
     });
 
-    usuario.save((errores, usuarioGuardado) => {
-        if (errores) {
+    usuario.save((error, usuarioGuardado) => {
+        if (error) {
             return respuesta.status(400).json({
                 ok: false,
                 mensaje: "Error al crear usuario en la base de datos",
-                errors: errores,
+                errors: error,
             });
         }
         res.status(201).json({
@@ -57,22 +63,24 @@ app.post("/", (req, res) => {
 app.put("/:id", (req, res) => {
     /* solo funciona con la libreria body parse */
     var id = req.params.id;
+    console.log("El id que se envio", id);
     Usuario.findById(id, (error, usuario) => {
-        if (errores) {
+        if (error) {
             return respuesta.status(500).json({
                 ok: false,
                 mensaje: "Error Base de Datos, Buscar usuario Actualizar",
-                errors: errores,
+                errors: error,
             });
         }
         if (!usuario) {
             return respuesta.status(400).json({
                 ok: false,
-                mensaje: "El usuario no existe",
-                errors: { message: 'No existe un usuario con ese ID' },
+                mensaje: `El usuario con el id ${id} no existe`,
+                errors: { message: "No existe un usuario con ese ID" },
             });
         }
         var body = req.body;
+        console.log(body);
 
         usuario.nombre = body.nombre;
         usuario.apellidos = body.apellidos;
@@ -92,8 +100,43 @@ app.put("/:id", (req, res) => {
                 body: usuarioGuardado,
             });
         });
-
     });
 });
+
+/* Borrar un usuario por id */
+
+app.delete('/:id', (req, respuesta) => {
+    var id = req.params.id;
+    Usuario.findByIdAndRemove(id, (error, usuarioBorrado) => {
+        if (error) {
+            return respuesta.status(500).json({
+                ok: false,
+                mensaje: "Error borrar usuario",
+                errors: error,
+            });
+        }
+        if (!usuarioBorrado) {
+            return respuesta.status(400).json({
+                ok: false,
+                mensaje: `El usuario con el id ${id} no existe para ser borrado`,
+                errors: { message: "No existe un usuario con ese ID" },
+            });
+        }
+        res.status(201).json({
+            ok: true,
+            body: usuarioBorrado,
+        });
+    });
+});
+
+
+/* Dum: Poder ver ruta solicitada, registrar peticiones que llegan al servidor
+MidleWare siempre se ejecuta antes de cualquier petición */
+function logger(req, res, next) {
+    console.log(
+        `Ruta Recibida :  ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    );
+    next();
+}
 
 module.exports = app;
