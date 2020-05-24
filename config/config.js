8baf0908ac42682daabc08d6737f81cb550cae7c
Darwin <darwinu34@yahoo.es>
2020-05-24 11:35:23 -0500

    logueo token google.

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'EstaEsLaSemillaParaCrearElTokenUnico-SenecesitaParaLeerloTambien';
+
+/* Google */
+module.exports.CLIENT_ID = '261803787485-9d77fs26rb1j6s2nd800t92bqm2t73mj.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -10,11 +10,52 @@ app.use(express.json());
 app.use(logger);
 var claveToken = require("../config/config");
 var Usuario = require("../models/usuario");
+/* google */
+const CLIENT_ID = require("../config/config").CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+/* Autentificación de google */
+//-------------------------------------------------------------
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true,
+    };
+}
+
+app.post('/google', async(req, respuesta) => {
+    var token = req.body.token;
+    var googleUser = await verify(token).catch(e => {
+        return respuesta.status(500).json({
+            ok: false,
+            mensaje: "Token no válido"
+        });
+    });
+    respuesta.status(200).json({
+        ok: true,
+        mensaje: 'Respuesta rapida funciona',
+        googleUser: googleUser
+    });
+});
+//-------------------------------------------------------------
+
 
 /* Dum: Login */
-
 app.post('/', (req, respuesta) => {
-
     var body = req.body;
     Usuario.findOne({ email: body.email }, (error, usuarioBaseDato) => {
 
