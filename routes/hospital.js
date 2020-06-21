844d5e63b24ac562e31ad5f062d0b0182d43e6e4
Darwin <darwinu34@yahoo.es>
2020-06-21 06:59:29 -0500

    se agrega la funcion de cargar x id

--- routes/hospital.js
+++ routes/hospital.js
@@ -10,6 +10,35 @@ app.use(logger);
 var Hospital = require("../models/hospital");
 var mdAutentificacion = require("../middlewares/autentificacion");
 
+// ==========================================
+// Obtener Hospital por ID
+// ==========================================
+app.get("/:id", (req, res) => {
+    var id = req.params.id;
+    Hospital.findById(id)
+        .populate("usuario", "nombre img email")
+        .exec((err, hospital) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: "Error al buscar hospital",
+                    errors: err,
+                });
+            }
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: "El hospital con el id " + id + "no existe",
+                    errors: { message: "No existe un hospital con ese ID" },
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital,
+            });
+        });
+});
+
 /* Obtener todos los hospitales */
 app.get("/", (request, respuesta, next) => {
     var desde = request.query.desde || 0;
@@ -17,27 +46,28 @@ app.get("/", (request, respuesta, next) => {
 
     /* DUM para que solo devuelva las columnas que se especifican excec */
     Hospital.find({})
-        .populate('usuario', 'nombre email') /* relacion con la tabla usuario y obtener sus campos */
+        .populate(
+            "usuario",
+            "nombre email"
+        ) /* relacion con la tabla usuario y obtener sus campos */
         .skip(desde)
         .limit(5)
-        .exec(
-            (error, hospitales) => {
-                if (error) {
-                    return respuesta.status(500).json({
-                        ok: false,
-                        mensaje: "Error cargando hospital base de datos",
-                        errors: error,
-                    });
-                }
-                Hospital.count({}, (error, cantidadRegistro) => {
-                    respuesta.status(200).json({
-                        ok: true,
-                        hospitales: hospitales,
-                        total: cantidadRegistro
-                    });
+        .exec((error, hospitales) => {
+            if (error) {
+                return respuesta.status(500).json({
+                    ok: false,
+                    mensaje: "Error cargando hospital base de datos",
+                    errors: error,
                 });
             }
-        );
+            Hospital.count({}, (error, cantidadRegistro) => {
+                respuesta.status(200).json({
+                    ok: true,
+                    hospitales: hospitales,
+                    total: cantidadRegistro,
+                });
+            });
+        });
 });
 
 /* Crear Hospital */
@@ -45,7 +75,7 @@ app.post("/", mdAutentificacion.verificarToken, (req, respuesta) => {
     var body = req.body;
     var hospital = new Hospital({
         nombre: body.nombre,
-        usuario: req.usuario._id
+        usuario: req.usuario._id,
     });
     hospital.save((error, hospitalGuardado) => {
         if (error) {
@@ -57,7 +87,7 @@ app.post("/", mdAutentificacion.verificarToken, (req, respuesta) => {
         }
         respuesta.status(201).json({
             ok: true,
-            hospital: hospitalGuardado
+            hospital: hospitalGuardado,
         });
     });
 });
@@ -103,7 +133,7 @@ app.put("/:id", mdAutentificacion.verificarToken, (req, respuesta) => {
 });
 
 /* Borrar un hospital por id */
-app.delete('/:id', mdAutentificacion.verificarToken, (req, respuesta) => {
+app.delete("/:id", mdAutentificacion.verificarToken, (req, respuesta) => {
     var id = req.params.id;
     Hospital.findByIdAndRemove(id, (error, hospitalBorrado) => {
         if (error) {
